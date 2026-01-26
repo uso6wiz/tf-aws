f052b349c60633c5512b2cc6291167c6caaaefbf
So Yamada <so.yamada@wiz.io>
2026-01-26 14:02:56 +0900

    Fix var

--- service/variables.tf
+++ service/variables.tf
@@ -36,7 +36,7 @@ variable "ecs_desired_count" {
 variable "github_org_repo_blog" {
   description = "uso8-blog の GitHub org/repo (e.g. myorg/uso8-blog-03)。デプロイ用 OIDC の trust に使用。"
   type        = string
-  default     = "uso6/uso8-blog-03"
+  default     = "uso6wiz/uso8-blog-03"
 }
 
 variable "github_branch_blog" {
